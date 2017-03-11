c0aff3509ababe67add99cb0570433bbdce87023
Hannah Schuster <hschuster9@gmail.com>
2017-03-11 13:43:12 -0500

    activities index view works now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
   # protect_from_forgery with: :exception
-    protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json'}
+    # protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json'}
 end
