7c7cb3dae016f5832e6163b4f00e288f0081450c
Nyketha Lyken <Nyketha.Lyken@gmail.com>
2017-03-13 12:02:36 -0400

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-<<<<<<< HEAD
-#   resources :people
-#   resources :activities
-#   resources :messages
-# end
-
-resources :activities do
-    resources :people
-  end
-
-  resources :people do
-    resources :messages
-  end
-=======
 
   resources :activities do
     resources :people
   end
   resources :messages
->>>>>>> master
 end
